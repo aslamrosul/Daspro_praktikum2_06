340fe0c32b6271ab10e55c5c7e36baa2bc5cccc8
aslamrosul <nvrsrndrmrx@gmail.com>
2023-09-08 14:35:01 +0700

    baris ke 3

--- MyFirstJavaGithub06.java
+++ MyFirstJavaGithub06.java
@@ -2,5 +2,6 @@ public class MyFirstJavaGithub06 {
    public static void main(String[] args) {
      System.out.println("Hello from GitHub");
      System.out.println("baris ke 2, perubahan");
+     System.out.println("baris 3");
    }
 }
